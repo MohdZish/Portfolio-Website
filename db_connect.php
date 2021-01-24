013dc6d2d57dec0d39064cab8a1c74b59077935d
Mohammed Zishan <mohdzish2000@gmail.com>
2021-01-24 23:49:07 +0100

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "new";
-	$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
-?>
